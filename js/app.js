4d7a22fbc8affb591f4e13025370e91b04e6608a
saich-shopmatic <chukka.sai@goshopmatic.com>
2020-01-09 01:12:22 +0530

    Authorizing the insta profile added.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,11 @@
-// Use the CDN or host the script yourself
-// https://cdnjs.cloudflare.com/ajax/libs/instafeed.js/1.4.1/instafeed.min.js
-// https://matthewelsom.com/assets/js/libs/instafeed.min.js
+const AUTH_URL = 'https://api.instagram.com/oauth/authorize?app_id=554472575139442&redirect_uri=https://saich-shopmatic.github.io/insta-feed/&scope=user_profile,user_media&response_type=code';
+const APP_ID = '554472575139442'
+const APP_SECRET = '70a56acaa155786732231f381d84a2e9' 
+const REDIRECT_URI = 'https://saich-shopmatic.github.io/insta-feed/'
+let code = ''
+let accessToken= ''
+let user_id = ''
 
-var userFeed = new Instafeed({
-    get: 'user',
-    userId: '8987997106',
-    clientId: '924f677fa3854436947ab4372ffa688d',
-    accessToken: '8987997106.924f677.8555ecbd52584f41b9b22ec1a16dafb9',
-    resolution: 'standard_resolution',
-    template: '<a href="{{link}}" target="_blank" id="{{id}}"><img src="{{image}}" /></a>',
-    sortBy: 'most-recent',
-    limit: 4,
-    links: false
-  });
-  userFeed.run();
-  
+function authUser() {
+    location.href = AUTH_URL
+}
