2c85aefb7b28e188c475cdcfa8db5056467a6c14
saich-shopmatic <chukka.sai@goshopmatic.com>
2020-01-09 17:08:22 +0530

    redirect_uri changed

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -1,8 +1,8 @@
 class InstagramController < ApplicationController
-AUTH_URL = 'https://api.instagram.com/oauth/authorize?app_id=554472575139442&redirect_uri=https://0ea94b25.ngrok.io/oauth/callback/&scope=user_profile,user_media&response_type=code';
+AUTH_URL = 'https://api.instagram.com/oauth/authorize?app_id=554472575139442&redirect_uri=https://saich-insta-feed.herokuapp.com/oauth/callback/&scope=user_profile,user_media&response_type=code';
 APP_ID = '554472575139442'
 APP_SECRET = '70a56acaa155786732231f381d84a2e9' 
-REDIRECT_URI = 'https://0ea94b25.ngrok.io/oauth/callback/'
+REDIRECT_URI = 'https://saich-insta-feed.herokuapp.com/oauth/callback/'
 GRANT_TYPE= 'authorization_code'
 ACCESSTOKEN_URL = 'https://api.instagram.com/oauth/access_token'
 
