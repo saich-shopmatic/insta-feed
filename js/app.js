9db4ca4d42e15992e385e6e8e283b40f85027e09
saich-shopmatic <58685990+saich-shopmatic@users.noreply.github.com>
2020-01-24 11:28:22 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const AUTH_URL = 'https://api.instagram.com/oauth/authorize?app_id=554472575139442&redirect_uri=https://saich-shopmatic.github.io/insta-feed/&scope=user_profile,user_media&response_type=code';
-const APP_ID = '554472575139442'
-const APP_SECRET = '70a56acaa155786732231f381d84a2e9' 
+const APP_ID = 'shopmatic-554472575139442'
+const APP_SECRET = 'shopmatic-70a56acaa155786732231f381d84a2e9' 
 const REDIRECT_URI = 'https://saich-shopmatic.github.io/insta-feed/'
 const GRANT_TYPE= 'authorization_code'
 const ACCESSTOKEN_URL = 'https://api.instagram.com/oauth/access_token'
